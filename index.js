ea46186ec1b64834f12c5df11a7d6b0f435e7331
William Stevens <wstevens.hornet@gmail.com>
2020-07-20 19:02:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+function writeCards(arr, str) {
+  for (let i = 0; i < arr.length; i++) {
+    console.log('Thank you, ${arr[i]}, for the wonderful birthday gift!');
+  }
+  
+}
+function countDown(posint) {
+  while(posint >= 0) {
+    console.log(posint)
+    posint--
+  }
+}
