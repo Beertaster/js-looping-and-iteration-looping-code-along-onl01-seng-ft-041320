f7c16034ffbe6c7909214a338da85d9c03acc32f
William Stevens <wstevens.hornet@gmail.com>
2020-07-20 19:10:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solutions in this file
 function writeCards(arr, str) {
   for (let i = 0; i < arr.length; i++) {
-    console.log("Thank you, ${arr[i]}, for the wonderful ${str} gift!");
+    new("Thank you, ${arr[i]}, for the wonderful ${str} gift!");
   }
   
 }
